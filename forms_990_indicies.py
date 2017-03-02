c3710745b1886c4dd4de6407f1eddd312efda607
Benjamin Chodoroff (bnchdrff) <anonyben@gmail.com>
2017-03-02 14:11:25 -0500

    make form_990_indicies more testable

--- forms_990_indicies.py
+++ forms_990_indicies.py
@@ -1,4 +1,3 @@
-import boto3
 from tempfile import NamedTemporaryFile
 import os
 import errno
@@ -9,33 +8,36 @@ CACHE_DIR = 'cached-data'
 
 BUCKET_NAME = 'irs-form-990'
 
-def make_cache_dir():
-    try:
-        os.makedirs(CACHE_DIR)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
-
 def get_only_json_filenames(indicies):
     return map(lambda f: f.get('Key'), filter(lambda e: e.get('Key').endswith('.json'), indicies.get('Contents')))
 
 class Forms990Indicies:
-    indicies = {}
-    saved_jsons = {}
+
+    def __init__(self, boto3_s3_resource):
+        self.indicies = {}
+        self.saved_jsons = {}
+        self.CACHE_DIR = CACHE_DIR
+        self.BUCKET_NAME = BUCKET_NAME
+        self.s3_resource = boto3_s3_resource
 
     def get_indicies(self):
-        s3 = boto3.resource('s3')
-        all_indicies = s3.meta.client.list_objects(Bucket=BUCKET_NAME, Prefix='index')
+        all_indicies = self.s3_resource.meta.client.list_objects(Bucket=BUCKET_NAME, Prefix='index')
         self.indicies = get_only_json_filenames(all_indicies)
 
+    def make_cache_dir(self):
+        try:
+            os.makedirs(self.CACHE_DIR)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+
     def save_s3_object(self, name):
-        make_cache_dir()
-        cached_file = os.path.join(CACHE_DIR, name)
+        self.make_cache_dir()
+        cached_file = os.path.join(self.CACHE_DIR, name)
 
         if not os.path.isfile(cached_file):
             fd = NamedTemporaryFile(delete=False)
-            resource = boto3.resource('s3')
-            bucket = resource.Bucket(BUCKET_NAME)
+            bucket = self.s3_resource.Bucket(self.BUCKET_NAME)
             bucket.download_file(name, fd.name)
             fd.close()
             copyfile(fd.name, cached_file)
@@ -45,7 +47,8 @@ class Forms990Indicies:
 
     def save_all_indicies(self):
         self.get_indicies()
-        map(lambda name: self.save_s3_object(name), self.indicies)
+        for name in self.indicies:
+          self.save_s3_object(name)
 
     def get_json_for_index(self, name):
         data = json.load(self.saved_jsons[name]).popitem()[1]
--- forms_990_indicies_test.py
+++ forms_990_indicies_test.py
@@ -1,8 +1,34 @@
 import unittest
+import mock
+import shutil
+import os
+import json
 
-from forms_990_indicies import get_only_json_filenames
+from boto3 import resource
+
+from forms_990_indicies import get_only_json_filenames, Forms990Indicies
+
+TESTING_CACHE_DIR = 'testing-cache-dir'
+
+class MockBucket():
+    def download_file(self, name, dest):
+        somedata = {
+          u'testdata': {
+            u'whatwewant': name
+          }
+        }
+        with open(dest, 'w') as fd:
+            json.dump(somedata, fd)
 
 class TestForms990Indicies(unittest.TestCase):
+
+    def setUp(self):
+        os.makedirs(TESTING_CACHE_DIR)
+
+    def tearDown(self):
+        shutil.rmtree(TESTING_CACHE_DIR)
+        return
+
     def test_get_only_json_filenames(self):
         only_jsons = get_only_json_filenames({u'Contents': [
           {u'Key': u'index1999.json'},
@@ -11,3 +37,31 @@ class TestForms990Indicies(unittest.TestCase):
         self.assertEqual([
           u'index1999.json',
         ], only_jsons)
+
+    def test_instantiation(self):
+        mock_s3_resource = mock.create_autospec(resource('s3'))
+        mock_s3_resource.Bucket.return_value = MockBucket()
+
+        indicies = Forms990Indicies(mock_s3_resource)
+
+        indicies.CACHE_DIR = TESTING_CACHE_DIR
+
+        mock_s3_resource.meta.client.list_objects.return_value = {u'Contents': [
+          {u'Key': u'index1999.json'},
+          {u'Key': u'index1999.csv'},
+          {u'Key': u'index2000.json'},
+          {u'Key': u'index2000.csv'},
+        ]}
+
+        indicies.get_indicies()
+        mock_s3_resource.meta.client.list_objects.assert_called_with(Bucket='irs-form-990', Prefix='index')
+
+        self.assertEqual([
+          u'index1999.json',
+          u'index2000.json',
+        ], indicies.indicies)
+
+        indicies.save_all_indicies()
+
+        self.assertEqual(indicies.get_json_for_index('index1999.json'), {'whatwewant': 'index1999.json'})
+        self.assertEqual(indicies.get_json_for_index('index2000.json'), {'whatwewant': 'index2000.json'})
